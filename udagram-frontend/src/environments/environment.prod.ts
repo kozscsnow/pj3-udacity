ae444a16378e0bb7cc71ce1e19b8526e1d1a4a3c
Nhat Khanh <nhatkhanh9a3@gmail.com>
2023-05-02 12:36:48 +0700

    replace env frontend v1a

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a92bae7e05de24620b262efaee3ca424-1058005566.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
